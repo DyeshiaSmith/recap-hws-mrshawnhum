920cc0302ca96eaa0b0ee68734e71b9580556ac3
Shawn Humphreys <shawnhum97@gmail.com>
2020-04-09 13:41:47 -0500

    Add homework solutions

---
+++ 48-hw.js
@@ -0,0 +1,25 @@
+// create a variable "timeOfDay" and assign it a string
+
+const timeOfDay = "Morning";
+
+// create a variable "greeting" that references a template literal
+// use "timeOfDay" in the template literal to create a message like "Good morning!" or "Good evening!"
+const greeting = `Good ${timeOfDay}`;
+
+// print "greeting"
+console.log(greeting);
+
+// create a new variable, but do not assign it a value
+let x;
+
+// print the variable and its type
+console.log(typeof x);
+
+// assign the variable a value that indicates the variable is purposely blank
+let y = {};
+
+// print the variable and its type again
+console.log(typeof y);
+
+// try to print a variable that does not exist
+console.log(skittles);
