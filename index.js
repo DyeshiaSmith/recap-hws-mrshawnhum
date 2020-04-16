64e695c4b45365c44b5ae98de05a8955838f8a26
Shawn Humphreys <shawnhum97@gmail.com>
2020-04-16 09:41:57 -0500

    Solved homework assignment using variables

--- index.js
+++ index.js
@@ -15,3 +15,11 @@ function remainder(num1, num2) {
 }
 
 // TODO: Using only the functions above, and no operators, calculate the value of tripling 5, multiplying that by 12, dividing by 2 and then finding the remainder of dividing that by 3.
+const tripleFive = tripler(5);
+console.log(tripleFive);
+const multiplyTwelve = multiply(tripleFive, 12);
+console.log(multiplyTwelve);
+const divideTwo = divide(multiplyTwelve, 2);
+console.log(divideTwo);
+const remainderOne = remainder(divideTwo, 3);
+console.log(remainderOne);
