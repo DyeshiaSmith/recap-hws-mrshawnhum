bf11adca0dab4f38dff8a0bfae5d3000eec9d2cb
Shawn Humphreys <shawnhum97@gmail.com>
2020-04-20 12:30:34 -0500

    Show examples of string methods using different methods and examples of math/ date methods using countdown timer

--- index.js
+++ index.js
@@ -3,17 +3,23 @@ string = string.replace("example", "knife");
 string = string.toUpperCase();
 console.log(string);
 
-const graduation = new Date(`June 26, 2020`);
+const graduation = new Date("June 26, 2020");
 const today = new Date(Date.now());
 
 function getRemainingDays() {
   let total = graduation - today;
+
   let seconds = Math.floor((total / 1000) % 60);
-  let minutes = Math.floor((total / 1000 / 60) % 60);
+  let minutes = Math.floor((total / (1000 * 60)) % 60);
   let hours = Math.floor((total / (1000 * 60 * 60)) % 24);
   let days = Math.floor((total / (1000 * 60 * 60 * 24)) % 7);
   let weeks = Math.floor(total / (1000 * 60 * 60 * 24 * 7));
-  return `${weeks}, ${days}`;
+
+  let countdownTimer = `${weeks} weeks, ${days} days, ${hours} hours, ${minutes} minutes, and ${seconds} seconds`;
+  if (total > 0) {
+    return `We have ${countdownTimer} left until we become full stack developers!`;
+  }
+  return "Congratulations! We have completed Claim Academy's Javascript boot camp and are now Full Stack Developers! Good luck to everyone on your journey!";
 }
 
 console.log(getRemainingDays());
