cddb7efb2dff835f6ecef7afc2985d09403928bf
Shawn Humphreys <shawnhum97@gmail.com>
2020-04-20 11:16:18 -0500

    Declare variables for timer

--- index.js
+++ index.js
@@ -5,5 +5,13 @@ console.log(string);
 
 const graduation = new Date(`June 26, 2020`);
 const today = new Date(Date.now());
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+const week = day * 7;
 
-console.log(today);
+function getRemainingDays() {
+  let total = graduation - today;
+  return total;
+}
